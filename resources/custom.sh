191d64cd630d9a94b23cb84606f687d5fe5bb40c
LoRd_MuldeR <mulder2@gmx.de>
2026-05-16 20:26:23 +0200

    Added the required dependencies + force update of installed packages.

--- resources/custom.sh
+++ resources/custom.sh
@@ -2,4 +2,20 @@
 
 set -exu
 
+export RUSTUP_HOME=/opt/rust/rustup
+export CARGO_HOME=/opt/rust/cargo
+
 # Add your additional provisioning here for custom VM images.
+
+pkg refresh
+pkg install build-essential git
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain 1.95.0
+
+tee /opt/rust/cargo/env <<'EOF'
+#!/bin/sh
+export PATH="/opt/rust/cargo/bin:$PATH"
+export RUSTUP_HOME=/opt/rust/rustup
+EOF
+
+chmod 555 /opt/rust/cargo/env
